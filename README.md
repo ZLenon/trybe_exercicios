d01da3ff74576584c730ca8a0a8268a0b13fa3c7
Lenon Nascimento <lenoninformatica@gmail.com>
2023-08-29 22:28:26 -0300

    configuração

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # trybe_exercicios
+
+- Instalação do ambiente virtual
+`python3 -m venv .venv`
+- Subir o ambiente virtual
+`source .venv/bin/activate`
+
 Exercícios propostos pela escola de programação TRYBE
---
+++ ciencia_da_computação/exercicio01.py
@@ -0,0 +1,12 @@
+# Exercício 1:
+# No terminal, inicialize duas variáveis a e b, sendo a = 10 e b = 5.
+# Mostre o resultado das 7 operações básicas (soma, subtração, multiplicação,
+# divisão, divisão inteira, potenciação e módulo) envolvendo essas variáveis.
+
+a = 10
+b = 5
+
+print("Soma {}".format(a + b))
+print("Subtração {}".format(a - b))
+print("Mutiplicação {}".format(a * b))
+print("Divisão {}".format(a / b))
