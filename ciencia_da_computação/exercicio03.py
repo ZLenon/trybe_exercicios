9a935b08c92b2b74f45c8248c35d9f7541d38b4e
Lenon Nascimento <lenoninformatica@gmail.com>
2023-08-30 14:52:18 -0300

    feat: exercicio_03

---
+++ ciencia_da_computação/exercicio03.py
@@ -0,0 +1,10 @@
+# Exercício 3:
+# Teste e verifique o que acontece se você colocar um ponto e vírgula no final
+# de uma instrução em Python.
+# Solução
+# Assim como em JavaScript, Python permite que você coloque um ponto e vírgula
+# no final de uma instrução.
+
+teste_ponto_virgula = 0
+
+print(teste_ponto_virgula)
