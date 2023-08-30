846bcfd58753ea68cf326c310713600a65b1e949
Lenon Nascimento <lenoninformatica@gmail.com>
2023-08-30 14:44:52 -0300

    feat: exercicio_02

---
+++ ciencia_da_computação/exercicio02.py
@@ -0,0 +1,16 @@
+# Exercício 2:
+# Declare e inicialize uma variável: hours = 6. Quantos minutos têm em 6 horas?
+# E quantos segundos? Declare e inicialize variáveis minutes e seconds que
+# recebem os respectivos resultados das contas. Depois, imprima cada uma delas.
+
+hours = 6
+minutes = 60 * hours
+seconds = 60 * minutes
+
+print(
+    """
+Em {} horas, tem {} minutos, e {} segundos
+""".format(
+        hours, minutes, seconds
+    )
+)
